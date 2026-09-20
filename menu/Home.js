fa64d0a0c226817ca9ed184298aaf49cd6160a4b
Yogesh <Yogeshdiploma@outlook.com>
2026-09-20 19:54:31 +0530

    Create Home.js

---
+++ menu/Home.js
@@ -0,0 +1,31 @@
+import "./Layout.css"
+import React from "react";
+
+function Home(){
+	return(
+		<React.Fragment>
+		<center>
+		<h1>Welcome to Government Polytechnic, Chamarajanagar</h1>
+		<p>
+		Welcome to the official digital gateway of Government Polytechnic, 
+		Chamarajanagar, a premier state-run technical institution dedicated 
+		to fostering academic excellence, technical innovation, 
+		and professional integrity. Established in 1984, our institute
+		 stands as a beacon of affordable, high-quality technical education,
+		  primarily empowering the rural youth of the Chamarajanagar and Mysuru
+		   districts in Karnataka. Fully approved by the All India Council for 
+		   Technical Education (AICTE), New Delhi, and functioning under the 
+		   administrative control of the Directorate of Technical Education (DTE), 
+		   Bengaluru, we are deeply committed to bridging the gap between rural talent 
+		   and global industrial demands. 
+		For over four decades, our campus has served as a 
+		nurturing ground where aspiring minds transform into 
+		skilled, employment-ready engineering professionals.
+		</p>
+		<h2 id="lt">-----------------------------------------------------------------------------------------------------------------------------</h2>
+		</center>
+		</React.Fragment>
+	);
+}
+
+export default Home;
