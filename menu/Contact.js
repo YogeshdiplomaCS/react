91d214636c5d414dd261a8fa75ead2edb680bceb
Yogesh <Yogeshdiploma@outlook.com>
2026-09-20 19:53:36 +0530

    Create Contact.js

---
+++ menu/Contact.js
@@ -0,0 +1,19 @@
+import "./Layout.css"
+import React from "react";
+
+function Contact(){
+	return(
+		<React.Fragment>
+		<center>
+		<h1>Contact Us: Connect With Our Campus</h1>
+		<p>
+		<b>Phone : </b> +91-8226-222579 <br />
+		<b> E-mail : </b> gptchnagar@gmail.com
+		</p>
+		<h2 id="lt">-----------------------------------------------------------------------------------------------------------------------------</h2>
+		</center>
+		</React.Fragment>
+	);
+}
+
+export default Contact;
