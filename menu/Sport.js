5f659deefd9db6eeb9a463d7f65e9c8b0a93339e
Yogesh <Yogeshdiploma@outlook.com>
2026-09-20 19:51:58 +0530

    Create Sport.js

---
+++ menu/Sport.js
@@ -0,0 +1,18 @@
+import "./Layout.css";
+import React from "react";
+
+function Sport(){
+	return(
+		<React.Fragment>
+		<center>
+		<h1>Sports & Athletics: Nurturing Health, Discipline, and Teamwork</h1>
+		<p>
+		The college boasts a spacious, well-maintained outdoor playground and indoor sports facilities that cater to a wide array of athletic disciplines. For students passionate about outdoor games, the campus features dedicated playing fields and courts for Cricket, Football, Volleyball, Kabaddi, Kho-Kho, and Ball Badminton. These facilities are equipped with the necessary kits and gear to ensure safe and professional training sessions. For indoor enthusiasts, the campus offers dedicated spaces for Table Tennis, Chess, and Carrom, providing a perfect avenue for students to de-stress and sharpen their mental focus between rigorous academic schedules. Under the professional guidance of our Physical Education Director, these facilities remain highly active throughout the academic year, buzzing with student energy and competitive spirit.
+		</p>
+		<h2 id="lt">-----------------------------------------------------------------------------------------------------------------------------</h2>
+		</center>
+		</React.Fragment>
+	);
+}
+
+export default Sport;
