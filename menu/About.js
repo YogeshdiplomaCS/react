10dedc7aa39d3b225be2c963c1f4aa85dae46073
Yogesh <Yogeshdiploma@outlook.com>
2026-09-20 19:52:18 +0530

    Add About component with institution details

---
+++ menu/About.js
@@ -0,0 +1,18 @@
+import "./About.css"
+import React from "react";
+
+function About(){
+	return(
+		<React.Fragment>
+		<center>
+		<h1> About Government Polytechnic, Chamarajanagar </h1>
+		<p>
+		Founded in the year 1984, Government Polytechnic, Chamarajanagar has established itself as one of the most trusted and prestigious technical institutes in Southern Karnataka. The college was set up by the Government of Karnataka with a visionary goal: to make high-quality engineering and technical education accessible to the rural and economically underprivileged sections of society. Over the past four decades, the institute has successfully broken geographical and financial barriers, transforming thousands of rural students into skilled technicians, entrepreneurs, and industrial leaders. Operating under the governance of the Directorate of Technical Education (DTE), Bengaluru, and proudly approved by the All India Council for Technical Education (AICTE), New Delhi, our institution serves as a benchmark for affordable academic excellence.
+	</p>
+	<h2 id="lt">-----------------------------------------------------------------------------------------------------------------------------</h2>
+	</center>
+		</React.Fragment>
+	);
+}
+
+export default About;
